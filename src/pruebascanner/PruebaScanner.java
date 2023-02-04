0962dd3aaf33741062fc4b055e200e982961dcb7
Tatiana Garcia <tzgarcia_16@unitec.edu>
2023-02-04 16:04:14 -0600

    Creacion de Proyecto

---
+++ src/pruebascanner/PruebaScanner.java
@@ -0,0 +1,48 @@
+package pruebascanner;
+
+import static java.lang.Math.random;
+import static java.lang.StrictMath.random;
+
+/**
+ * @author tatig
+ */
+public class PruebaScanner {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        while !dead: 
+            if(homework==0||difficulty<50){
+                for (int i = 0; i < 1; i++) {
+                    eat();
+                }    
+            }
+            for (int i = 0; i < Infinity; i++) {
+                code();
+                int exception_in_code = random.nextInt(99);
+                String social_life = null;
+                //sleep;
+            } 
+            int grade  = random.nextInt(65);    
+            switch(nota){
+                case <60:
+                    Carrera nueva = new Carrera();
+                    nueva.add(depression)
+                break;
+            }  
+            
+            
+           
+            
+            
+            
+            
+            
+            
+            System.out.println(grade+exception_in_code);
+            System.out.println("");
+            
+    }
+}
